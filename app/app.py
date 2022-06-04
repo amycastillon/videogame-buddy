f738ebca7059741ff3b149a7360f8f29d6b34f5f
Rachel Dowdy <racheladowdy@gmail.com>
2022-06-04 10:53:07 -0500

    reorganize images/css

--- app/app.py
+++ app/app.py
@@ -11,5 +11,19 @@ app.static_folder = 'static'
 def home():
     return render_template('index.html')
 
+@app.route('/videogame_buddy.html')
+def vgbuddy():
+    return render_template('videogame_buddy.html')
+
+@app.route('/browse_games.html')
+def browse():
+    return render_template('browse_games.html')
+
+@app.route('/results.html')
+def results():
+    return render_template('results.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
