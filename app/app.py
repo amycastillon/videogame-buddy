ab2d5ce6bada667522f2b21abf5efcc2038ee574
JWatson1102 <irishwatsonj>
2022-06-04 11:19:57 -0500

    working on backend video game suggestion code

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
+from crypt import methods
 import sqlite3
 from flask import Flask, jsonify, render_template
+from flask import redirect, url_for, request
 import os
 import sqlite3
 
@@ -10,6 +12,10 @@ app.static_folder = 'static'
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/filter_search',methods = ['POST','GET'])
+def search(submission):
+    return jsonify(submission)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/static/js_files/questionnaire.js
+++ app/static/js_files/questionnaire.js
@@ -5,27 +5,23 @@ var button = d3.select("#submit_button");
 function handleClick() {
     console.log("A button was clicked!");
 
-    let submission = [];
-
-    // We can use d3 to see the object that dispatched the event
-    // console.log(d3.event.target);
-
-    // let platform = d3.select('#dropdown option:checked').text();
-
-    // console.log(d3.select("gameinput").text())
+    let submission = {};
 
     let title = document.getElementById("gameinput").value;
     console.log(title);
+    submission.title = title;
 
     let esrb_checks = document.getElementsByClassName("esrb_checkbox");
 
     let esrb_labels = document.getElementsByClassName("esrb_label");
 
-    // console.log(esrb_checks)
+    submission.esrb = [];
 
     for(let i = 0; i < esrb_checks.length; i++) {
         if(esrb_checks[i].checked) {
-            console.log(esrb_labels[i].textContent)
+            let esrb_rating = esrb_labels[i].textContent;
+            console.log(esrb_rating);
+            submission.esrb.push(esrb_rating);
         }
     };
 
@@ -33,66 +29,46 @@ function handleClick() {
 
     let platform_labels = document.getElementById("plaform_checklist").getElementsByClassName("form-check-label")
 
+    submission.platform = [];
+
     for(let i = 0; i < platform_checks.length; i++) {
         if(platform_checks[i].checked) {
-            console.log(platform_labels[i].textContent)
+            game_platform = platform_labels[i].textContent;
+            console.log(game_platform);
+            submission.platform.push(game_platform);
         }
     };
 
     let genre_checks = document.getElementById("genre_checklist").getElementsByClassName("form-check-input")
     let genre_labels = document.getElementById("genre_checklist").getElementsByClassName("form-check-label")
 
+    submission.genre = [];
+
     for(let i = 0; i < genre_checks.length; i++) {
         if(genre_checks[i].checked) {
-            console.log(genre_labels[i].textContent)
+            let game_genre = genre_labels[i].textContent;
+            console.log(game_genre);
+            submission.genre.push(game_genre);
         }
     };
 
-    // let esrb_checks = document.querySelectorAll('esrb');
+    console.log(submission);
 
-    // console.log(esrb_checks)
-
-    // let esrb_selections = [];
-
-    // for(let i=0; i<esrb_checks.length;i++) {
-    //     let check_check = esrb_checks.item(i);
-    //     console.log(check_check);
-    // }
-
-    // console.log(checks);
-
-    // let checkboxes = [check1,check2,check3];
-    
-    // for (let i=0; i< checks.length; i++) {
-    //     if (checks[i].checked) {
-    //         if(i==0) {
-    //             esrb_selections.push('E');
-    //         }
-    //         if(i==1) {
-    //             esrb_selections.push('T');
-    //         }
-    //         if(i==2) {
-    //             esrb_selections.push('M');
-    //         }
-    //     }
-    // }
-
-    // submission.push(platform);
-    // submission.push(title);
-    // submission.push(esrb_selections);
-
-    // console.log(submission);
-
-    // fetch("/data-search", {
-    //     method: "POST",
-    //     headers: {'Content-Type': 'application/json'},
-    //     body: JSON.stringify(submission)
-    // })
-    // .then(response => response.json())
-    // .then(function (json) {
-        
-    //     }
-    // );
+    fetch("/filter_search", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(submission)
+    })
+    .then(response => response.json())
+    .then(submission => {
+        console.log('Sucess',data);
+    })
+    .then(function (game_suggestion) {
+        console.log(game_suggestion)
+    })
+    .catch((error) => {
+        console.error('Error',error)
+    })
 }
 
 button.on("click", handleClick);
